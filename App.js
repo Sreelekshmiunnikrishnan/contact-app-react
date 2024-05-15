dc90be78f5ec4e6c475e5cbe8405440ed631512e
Sreelekshmiunnikrishnan <150163979+Sreelekshmiunnikrishnan@users.noreply.github.com>
2024-05-15 23:04:07 +0400

    first commit

---
+++ App.js
@@ -0,0 +1,25 @@
+
+import './App.css';
+import React from 'react';
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import ContactList from './components/ContactList';
+ import Header from './components/Header';
+ import CreateContact from './components/CreateContact';
+ import EditContact from './components/EditContact';
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <Router>
+        <Routes>
+        <Route path ="/" element={<ContactList></ContactList>}></Route>
+        <Route path ="/create" element={<CreateContact></CreateContact>}></Route>
+        <Route path ="/edit" element={<EditContact></EditContact>}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
+
