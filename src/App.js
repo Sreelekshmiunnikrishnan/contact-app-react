a131e0ad2d6d2ecba2a041133f036e88dbc21c67
Sreelekshmiunnikrishnan <150163979+Sreelekshmiunnikrishnan@users.noreply.github.com>
2024-05-16 00:08:52 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
 import React from 'react';
-import {HashRouter,Routes,Route } from 'react-router-dom';
+import {HashRouter as Router,Routes,Route } from 'react-router-dom';
 import ContactList from './components/ContactList';
  import Header from './components/Header';
  import CreateContact from './components/CreateContact';
@@ -10,16 +10,14 @@ function App() {
   return (
     <div className="App">
       <Header></Header>
-   <HashRouter>
-      <Router>
+       <Router>
         <Routes>
         <Route path ="/" element={<ContactList></ContactList>}></Route>
         <Route path ="/create" element={<CreateContact></CreateContact>}></Route>
         <Route path ="/edit" element={<EditContact></EditContact>}></Route>
         </Routes>
       </Router>
-   </HashRouter>
-    </div>
+  </div>
   );
 }
 
