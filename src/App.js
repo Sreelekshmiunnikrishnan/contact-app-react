2d9a9f0756a1dea7d3fffd832ffc1d9e2fb54fa9
Sreelekshmiunnikrishnan <150163979+Sreelekshmiunnikrishnan@users.noreply.github.com>
2024-05-18 21:57:35 +0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,27 +2,29 @@
 import './App.css';
 import React from 'react';
 import {HashRouter as Router,Routes,Route } from 'react-router-dom';
-import ContactList from '../components/ContactList';
+import ContactList from './components/ContactList';
  import Header from './components/Header';
  import CreateContact from './components/CreateContact';
  import EditContact from './components/EditContact';
+import users from './users';
 function App() {
   return (
     <div className="App">
       <Header></Header>
+   HI....
      <ContactList></ContactList>
        <Router>
          <Routes>
    
           {/* Homepage - Contact List */}
-         <Route path="contact-app-react/" element={<ContactList/>} />
+         <Route path="/" element={<ContactList/>} />
 
 
           {/* Route for creating a new contact */}
-          <Route path="contact-app-react/create" element={<CreateContact/>} />
+          <Route path="/create" element={<CreateContact/>} />
 
           {/* Route for editing an existing contact */}
-          <Route path="contact-app-react/edit" element={<EditContact/>} />
+          <Route path="/edit" element={<EditContact/>} />
         </Routes>
       </Router>
     </div>
